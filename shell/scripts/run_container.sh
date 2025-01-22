0149dad42965c385ac979bee554825015cfb7ba6
Julian Knocke <352838+JHK@users.noreply.github.com>
2025-01-22 13:55:30 +0100

    Args for kubeshell

--- shell/scripts/run_container.sh
+++ shell/scripts/run_container.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cd "$(dirname "$0")"
+
 CONTEXT=$(kubectl config current-context)
 NAMESPACE=$(kubectl config view --minify --output 'jsonpath={..namespace}')
 NAME=${USER//\./-}-shell
-case $(basename "$SHELL") in
-  bash) COMMAND=bash;;
-  fish) COMMAND=fish;;
-  *)    COMMAND=fish;;
+case "$#" in
+  0) COMMAND=fish;;
+  1) COMMAND="$1";;
+  *) echo "Usage: $0 [COMMAND]"; exit 1;;
 esac
 
-echo
-echo "Starting shell container pod:"
-echo ""
-echo "Context:   $CONTEXT"
-echo "Namespace: $NAMESPACE"
-echo "Pod name:  $NAME"
-echo "Command:   $COMMAND"
-echo ""
+>&2 echo
+>&2 echo "Starting shell container pod:"
+>&2 echo ""
+>&2 echo "Context:   $CONTEXT"
+>&2 echo "Namespace: $NAMESPACE"
+>&2 echo "Pod name:  $NAME"
+>&2 echo "Command:   $COMMAND"
+>&2 echo ""
 read -rp "Continue (Y/n)? " choice
 case "$choice" in
   n|N ) exit 1;;
 esac
 
-echo
-cat pod.yaml | sed "s/__NAME__/$NAME/g" | sed "s/__NAMESPACE__/$NAMESPACE/g" | kubectl apply --context $CONTEXT -f -
+>&2 echo
+cat ../pod.yaml | sed "s/__NAME__/$NAME/g" | sed "s/__NAMESPACE__/$NAMESPACE/g" | >&2 kubectl apply --context $CONTEXT -f -
 
-./scripts/wait_for_container.sh "$CONTEXT" "$NAMESPACE" "$NAME"
+./wait_for_container.sh "$CONTEXT" "$NAMESPACE" "$NAME"
 
 kubectl exec -it --context $CONTEXT --namespace "$NAMESPACE" "$NAME" -- "$COMMAND" || true
 
-kubectl delete pod --context $CONTEXT --namespace "$NAMESPACE" "$NAME"
+>&2 kubectl delete pod --context $CONTEXT --namespace "$NAMESPACE" "$NAME"
--- shell/scripts/wait_for_container.sh
+++ shell/scripts/wait_for_container.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-echo
-echo -n "waiting for pod to start ..."
+>&2 echo
+>&2 echo -n "waiting for pod to start ..."
 while true; do
   retVal=$(kubectl get pods --context "$1" --namespace "$2" --field-selector=status.phase=Running 2>&1 | grep "$3")
   if [ $? -eq 0 ]; then
-    echo " attaching"
-    echo
+    >&2 echo " attaching"
+    >&2 echo
     exit 0
   else
-    echo -n "."
+    >&2 echo -n "."
     sleep 1
   fi
 done
